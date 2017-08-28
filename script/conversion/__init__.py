2792e7b047e5e342418dcc6ecb2943aa3d8ac25b
ClementLancien <clement.lancien@gmail.com>
2017-08-28 18:09:55 +0200

    __init__.py

---
+++ script/conversion/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+from accession import Accession
+from ensembl import Ensembl
+from gpl import GPL
+from history import History
+from homologene import Homologene
+from info import Info
+from swissprot import Swissprot
+from trembl import TREMBL
+from unigene import Unigene
+from vega import Vega
+
+
